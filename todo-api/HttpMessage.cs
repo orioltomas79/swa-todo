eb8baede595bedbbf97abc2e4af5c617361956d1
Oriol Tomas <oriol.tomas@benchmarkcapital.co.uk>
2023-06-22 21:55:29 +0200

    UI: Generate API client

--- todo-api/HttpMessage.cs
+++ todo-api/HttpMessage.cs
@@ -1,18 +1,32 @@
-using System;
 using System.IO;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 namespace Todo.Api
 {
-    public static class HttpMessage
+    public class Message
     {
+        private readonly ILogger<Message> _logger;
+
+        public Message(ILogger<Message> log)
+        {
+            _logger = log;
+        }
+
         [FunctionName("Message")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
@@ -21,3 +35,4 @@ namespace Todo.Api
         }
     }
 }
+
