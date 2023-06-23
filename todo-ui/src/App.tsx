46ca938c3e48639265cf0bcdfc3b0fb820515884
Oriol Tomas <oriol.tomas@benchmarkcapital.co.uk>
2023-06-23 14:54:34 +0200

    UI: Add NSwag to dev dependencies

--- todo-ui/src/App.tsx
+++ todo-ui/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import { Client } from "./api/apiClient.g.nswag";
 
 function App() {
